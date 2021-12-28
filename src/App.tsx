e7041504b744afbfffcc6a827039870546943b5b
Andrew Oetegenn <andrewoetegenn@gmail.com>
2021-12-28 13:13:43 +0000

    Commit to test automated CDK deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import './App.css';
 
-const App = () => <h1>Personal Finance App</h1>
+const App = () => (
+    <>
+        <h1>Personal Finance App</h1>
+        <p>This application displays personal finannce information</p>
+    </>
+)
 
 export default App;
